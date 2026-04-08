a6d5de018c55d46665780b781c5db33c7cae7c90
ThLandi3 <92590808+ThLandi3@users.noreply.github.com>
2026-04-08 16:53:53 +0200

    Delete stackblitz-starters-htrovn5x directory

--- stackblitz-starters-htrovn5x/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  'https://nlolefwzuzkuonxragro.supabase.co',
-  'sb_publishable_gkZfwbJmXUoWpRm2xTUDkQ_2oonao6t'
-);
